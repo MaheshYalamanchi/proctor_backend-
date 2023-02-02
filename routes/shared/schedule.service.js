cba2def80ff0be4b653d1b2cf16a16e3670ec17b
mahesh <mahesh.yalamnchi@lntecc.com>
2023-02-02 11:37:56 +0530

    bug fixes
    to get the proper response where type = "face"

--- routes/shared/schedule.service.js
+++ routes/shared/schedule.service.js
@@ -97,42 +97,81 @@ let getCandidateMessages = async (params) => {
                 url: process.env.MONGO_URI,
                 client: "chats",
                 docType: 1,
-                query: [
-                    {
-                        "$match": {
-                            "room": params.params.roomId,
-                            "type": { "$regex": params.query.filter.type, "$options": 'i' }
-                        }
-                    },
-                    {
-                        "$lookup": {
-                            "from": 'users',
-                            "localField": 'user',
-                            "foreignField": '_id',
-                            "as": 'data',
-                        }
-                    },
-                    { "$unwind": { "path": "$data", "preserveNullAndEmptyArrays": true } },
-                    {
-                        "$project": {
-                            "attach": 1, "createdAt": 1, "_id": 0, "metadata": 1, "room": 1, "type": 1, "id": "$_id",
-                            "user": {
-                                "id": "$data._id",
-                                "nickname": "$data.nickname",
-                                "role": "$data.role",
-                                "username": "$data._id"
+                query:[
+                        {
+                            "$match": {
+                                "room": params.params.roomId,
+                                "type": { "$regex": params.query.filter.type, "$options": 'i' }
                             }
-                        }
-                    },
-                    {
-                        $sort: { createdAt: sort }
-                    },
-                    { "$limit": limit }
-                ]
+                        },
+                        {
+                            "$lookup": {
+                                "from": 'users',
+                                "localField": 'user',
+                                "foreignField": '_id',
+                                "as": 'data',
+                            }
+                        },
+                        { "$unwind": { "path": "$data", "preserveNullAndEmptyArrays": true } },
+                        {
+                            "$project": {
+                                "attach": 1, "createdAt": 1, "_id": 0, "metadata": 1, "room": 1, "type": 1, "id": "$_id",
+                                "user": {
+                                    "id": "$data._id",
+                                    "nickname": "$data.nickname",
+                                    "role": "$data.role",
+                                    "username": "$data._id"
+                                }
+                                
+                            }
+                        },
+                        { "$unwind": { "path": "$attach", "preserveNullAndEmptyArrays": true } },
+                        {
+                            "$lookup": {
+                                "from": 'attaches',
+                                "localField": 'attach',
+                                "foreignField": '_id',
+                                "as": 'attaches',
+                            }
+                        },
+                        { "$unwind": { "path": "$attaches", "preserveNullAndEmptyArrays": true } },
+                        {
+                            "$project": {
+                                "type":"$type","createdAt": "$createdAt", "metadata": "$metadata", "room": "$room", "id":"$id",
+                                "user": {
+                                            "id": "$user.id",
+                                            "nickname": "$user.nickname",
+                                            "role": "$user.role",
+                                            "username": "$user.id"
+                                        },
+                                "attach":[
+                                            {
+                                                "filename":"$attaches.filename",
+                                                "mimetype":"$attaches.mimetype",
+                                                "id":"$attaches._id"
+                                            }
+                                         ]
+                            }
+                        },
+                        {
+                            "$facet": {
+                                "data": [
+                                    { "$sort": { "createdAt": sort } },
+                                    { "$limit": limit }
+                                ],
+                                "total_count": [
+                                    { "$group": { _id: null, "count": { "$sum": 1 } } },
+                                    { "$project" : {_id:0 }}
+                                ]
+                            }
+                        },
+                        { "$unwind": { "path": "$total_count", "preserveNullAndEmptyArrays": true } },
+                        {"$project":{"data":"$data","total":"$total_count.count"}}
+                        ]
             };
             let responseData = await invoke.makeHttpCall("post", "aggregate", getdata);
             if (responseData && responseData.data && responseData.data.statusMessage) {
-                return { success: true, message: responseData.data.statusMessage }
+                return { success: true, message: responseData.data.statusMessage[0] }
             } else {
                 return { success: false, message: 'Data Not Found' }
             }
