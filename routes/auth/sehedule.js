43bd81b95d05e034a201cdcd23adfc47e5936022
varatharajan29062001 <121555475+varatharajan29062001@users.noreply.github.com>
2023-05-31 12:23:20 +0530

    reportlog api integration

--- routes/auth/sehedule.js
+++ routes/auth/sehedule.js
@@ -477,6 +477,29 @@ let chatincidents = async(params) => {
         }
     }
 }
+let fetchdata = async (params) => {
+    try {
+        var userdata = {
+            url:process.env.MONGO_URI,
+            database:"proctor",
+            model: "rooms",
+            docType: 1,
+            query: params.filter
+        }
+        let response = await invoke.makeHttpCall("post", "read", userdata);
+        if (response) {
+            return response;
+        } else {
+            return "Data Not Found";
+        }
+    } catch (error) {
+        if (error && error.code == 'ECONNREFUSED') {
+            return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+        } else {
+            return { success: false, message: error }
+        }
+    }
+};
 
 module.exports = {
     roomUserDetails,
@@ -491,5 +514,6 @@ module.exports = {
     attachCall,
     getTemplate,
     updateTemplate,
-    chatincidents
+    chatincidents,
+    fetchdata
 }
