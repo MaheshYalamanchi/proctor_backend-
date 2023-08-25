89a8aed4deed54a24b5db36087c84165d767f5c8
mahesh <mahesh.yalamnchi@lntecc.com>
2023-08-25 20:54:19 +0530

    sessions api uncommented

--- routes/auth/sehedule.js
+++ routes/auth/sehedule.js
@@ -498,29 +498,29 @@ let chatincidents = async(params) => {
         }
     }
 }
-// let fetchdata = async (params) => {
-//     try {
-//         var userdata = {
-//             url:process.env.MONGO_URI,
-//             database:"proctor",
-//             model: "rooms",
-//             docType: 1,
-//             query: params.filter
-//         }
-//         let response = await invoke.makeHttpCall("post", "read", userdata);
-//         if (response) {
-//             return response;
-//         } else {
-//             return "Data Not Found";
-//         }
-//     } catch (error) {
-//         if (error && error.code == 'ECONNREFUSED') {
-//             return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
-//         } else {
-//             return { success: false, message: error }
-//         }
-//     }
-// };
+let fetchdata = async (params) => {
+    try {
+        var userdata = {
+            url:process.env.MONGO_URI,
+            database:"proctor",
+            model: "rooms",
+            docType: 1,
+            query: params.filter
+        }
+        let response = await invoke.makeHttpCall("post", "read", userdata);
+        if (response) {
+            return response;
+        } else {
+            return "Data Not Found";
+        }
+    } catch (error) {
+        if (error && error.code == 'ECONNREFUSED') {
+            return { success: false, message: globalMsg[0].MSG000, status: globalMsg[0].status }
+        } else {
+            return { success: false, message: error }
+        }
+    }
+};
 
 module.exports = {
     roomUserDetails,
@@ -536,5 +536,5 @@ module.exports = {
     getTemplate,
     updateTemplate,
     chatincidents,
-    // fetchdata
+    fetchdata
 }
